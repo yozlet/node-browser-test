734d8194b91837b05a83bbb9a3fc7d2e92975f7f
Christopher Tarquini <code@tarq.io>
2021-09-13 14:20:48 -0400

    proxy with nginx in docker

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// the sdk wants this
+window.setImmediate = (fn) => setTimeout(fn,0)
 const LaunchDarkly = require('launchdarkly-node-server-sdk');
-const sdk_key = ""; // remember, it's SDK key, NOT client ID
-const client = LaunchDarkly.init(sdk_key);
+const sdk_key = process.env.LD_SDK_KEY; // remember, it's SDK key, NOT client ID
+const client = LaunchDarkly.init(sdk_key, {
+  streamUri: "http://localhost:1234/proxy/stream/",
+  baseUri: "http://localhost:1234/proxy/app/",
+  eventsUri: "http://localhost:1234/proxy/events/"
+});
 
 client.once("ready", () => {
-    client.variation("pricing-tier-3", {"key": "user@test.com"}, false,
-      (err, showFeature) => {
-        if (showFeature) {
-          console.log("Flag is true!");
-        } else {
-          console.log("Flag is false!");
-        }
-      });
+    client.allFlagsState({"key": "user@test.com"})
+      .then(
+          (flags) => console.log(flags.toJSON()),
+          (e) => console.error(e)
+       )
+    
 });
